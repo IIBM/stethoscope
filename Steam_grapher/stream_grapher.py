ab494a416a3f7b4980c721d7ef3e5a151d6ee22c
Chikyjudas <brian_san_92@hotmail.com>
2015-09-25 01:19:53 -0300

    Steam_grapher

---
+++ Steam_grapher/stream_grapher.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright (c) 2009 Piccinini Santiago
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+''' A "real-time" stream grapher.
+'''
+from stream_widgets import StreamWidget
+
+#subjectID= raw_input("Ingrese el nombre del paciente: ")
+subjectID="Brian"
+#backend = raw_input("Choose backend: 1 = Spiro,  2 = x^3: ")
+
+#Acá elijo el backend para que Mario no lo tenga que tipear
+backend= "1"
+if backend == "1":
+    from backends.spiro_com import Spiro
+    #com = raw_input("COM port:")
+    com="COM4"   #ttyACM0 para la compu con Ubuntu
+    if com == "":
+        spiro = Spiro(timeout=0.5)
+    else :
+        spiro = Spiro(port=com, timeout=0.5)
+    spiro.run()
+    backend = "spiro"
+    out_file_name = subjectID+ "_dataECG.log"
+    out_file= open(out_file_name, "w")
+    from filters.iir_filter import IIRFilter
+    filt_45hz_2nd = IIRFilter((1.0, 2.0, 1.0),(1.0, -1.6692031429, 0.7166338735))
+    
+else:
+    backend = "math"
+    
+
+import pyglet
+from pyglet.window import key
+import random, math
+
+import ctypes
+user32 = ctypes.windll.user32
+
+SIZE = (user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
+N_SAMPLES = 1500
+
+config = pyglet.gl.Config(double_buffer=True, buffer_size=24)
+window = pyglet.window.Window(SIZE[0], SIZE[1], config=config)
+#window.set_vsync(False)
+fps_display = pyglet.clock.ClockDisplay()
+
+
+stream_widget1 = StreamWidget(N_SAMPLES, (SIZE[0]-400, SIZE[1]-100), (100,100))
+
+
+@window.event
+def on_draw():
+    window.clear()
+    stream_widget1.redraw()
+    fps_display.draw()
+
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    pass
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.RIGHT: # Increase sample per screen in 20%
+        n_samples = stream_widget1.graph.n_samples
+        stream_widget1.graph.set_n_samples(int(math.ceil(n_samples + n_samples*0.2 )))
+    elif symbol == key.LEFT: # Decrease sample per screen in 20%
+        n_samples = stream_widget1.graph.n_samples
+        new_n_samples = int(n_samples - n_samples*0.2)
+        if new_n_samples > 1:
+            stream_widget1.graph.set_n_samples(new_n_samples)
+    elif symbol == key.UP:
+        old_amplification = stream_widget1.graph.amplification
+        stream_widget1.graph.set_amplification(old_amplification + old_amplification * 0.4)
+    elif symbol == key.DOWN:
+        old_amplification = stream_widget1.graph.amplification
+        stream_widget1.graph.set_amplification(old_amplification - old_amplification * 0.4)
+
+
+
+def update(dt):
+    if backend == "math":
+        stream_widget1.graph.add_samples([t**3/4.0 for t in range(-10,11)])
+    elif backend == "spiro":
+        samples = spiro.get_remaining_samples()
+        #print samples, len(samples)
+        out_file.write("\n".join([str(sample) for sample in samples])+"\n")
+        #stream_widget1.graph.add_samples([filt_45hz_2nd(sample)/90.0 for sample in samples])
+        stream_widget1.graph.add_samples([sample/90.0 for sample in samples])
+        
+
+
+pyglet.clock.schedule_interval(update, 0.05)
+
+pyglet.app.run()
---
+++ Steam_grapher/stream_widgets.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright (c) 2009 Piccinini Santiago
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import itertools
+import pyglet.graphics
+sample_rate=1000
+
+
+def from_iterable(iterables):
+    # chain.from_iterable(['ABC', 'DEF']) --> A B C D E F
+    for it in iterables:
+        for element in it:
+            yield element
+
+def flatten(listOfLists):
+    return list(itertools.chain(from_iterable(listOfLists)))
+
+class Grid(object):
+    def __init__(self, size, position, color=(100,255,100), h_sep=50, v_sep=50):
+
+        self.size = size
+        self.position = position
+        self.color = color
+        self.h_sep = h_sep
+        self.v_sep = v_sep
+
+        v_center = position[1] + self.size[1]/2       
+        auxs = range(0, size[1]/2+1, v_sep) + range(0,-size[1]/2-1,-v_sep)[1:] #acá me armo una secuencia de -screenHeight/2 a ScreenHeight/2 con pasos v_sep
+        num_h_lines = len(auxs)
+        h_vertexs = flatten([(position[0], y + v_center, position[0]+size[0], y + v_center) for y in auxs])
+
+        num_v_lines = size[0]/ h_sep + 1         
+        v_vertexs = flatten([(position[0]+x*h_sep, position[1], position[0]+x*h_sep, position[1]+size[1]) for x in range(num_v_lines)])
+        
+        h_colors = flatten([self.color for x in range(num_h_lines*2)])
+        v_colors = flatten([self.color for x in range(num_v_lines*2)])
+        self.h_vertex_list = pyglet.graphics.vertex_list(num_h_lines*2, ('v2f\static', h_vertexs), ("c3B\static", h_colors))
+        self.v_vertex_list = pyglet.graphics.vertex_list(num_v_lines*2 , ('v2f\static', v_vertexs), ("c3B\static", v_colors))
+        
+    def draw(self):
+        self.h_vertex_list.draw(pyglet.gl.GL_LINES)
+        self.v_vertex_list.draw(pyglet.gl.GL_LINES)
+
+class StreamGraph(object):
+    def __init__(self, n_samples, size, position, color=(255,255,255), amplification=5):
+        self.n_samples = n_samples
+        self.samples = [0] * n_samples
+        self.actual_sample_index = 0
+        self.width = size[0]
+        self.heigth = size[1]
+        self.position = position
+        self.color = color
+        self.amplification = amplification
+        
+        vertexs = self._vertex_list_from_samples(self.samples)
+        colors = flatten([self.color for x in range(n_samples)])
+        self._vertex_list = pyglet.graphics.vertex_list(n_samples, ('v2f\stream', vertexs), ("c3B\static", colors))
+        
+        self.grid = Grid(size, position, h_sep=50, v_sep=50)
+        
+        self.samples_per_h_division = int(self.n_samples * float(self.grid.h_sep) / float(self.width))
+        self.samples_per_h_division_label = pyglet.text.Label(str(float(self.samples_per_h_division*1000/sample_rate))+ "mseg/div",
+                          font_size=14, x=size[0]/2.0 + position[0], y=position[1]- 10, anchor_x='center', anchor_y='center')
+                          
+        self.values_per_v_division = int(self.grid.v_sep / float(self.amplification))
+        self.values_per_v_division_label = pyglet.text.Label(str(self.values_per_v_division)+"/div",
+                          font_size=14, x=position[0]-40, y=position[1]+self.heigth/2.0, anchor_x='center', anchor_y='center')
+
+    
+    def draw(self, samples):
+        "Add a list of samples to the graph and then draw it"
+        self.add_samples(samples)
+        self.grid.draw()
+        self._vertex_list.draw(pyglet.gl.GL_LINE_STRIP)
+        self.samples_per_h_division_label.draw()
+        self.values_per_v_division_label.draw()
+
+    def redraw(self):
+        "Draw the graph"
+        self.grid.draw()
+        self._vertex_list.draw(pyglet.gl.GL_LINE_STRIP)
+        self.samples_per_h_division_label.draw()
+        self.values_per_v_division_label.draw()
+        
+
+    def add_samples(self, samples):
+        "Add a list of samples to the graph"
+        for sample in samples:
+            index = self.actual_sample_index
+            self.samples[index] = sample
+            self._vertex_list.vertices[index*2:index*2+2] = self._vertex_from_sample(sample, index)
+            self.actual_sample_index +=1
+            if self.actual_sample_index >= self.n_samples:
+                    self.actual_sample_index = 0
+
+    def set_n_samples(self, n_samples):
+        "Set a new value of n_samples"
+        if n_samples <=0:
+            raise AttributeError("n_samples must be > 0")
+        self.n_samples = n_samples
+        new_samples = [0]*n_samples
+        for i in range(n_samples):
+            try:
+                new_samples[i] = self.samples[i]
+            except IndexError:
+                break
+        self.samples = new_samples
+        self._vertex_list.resize(n_samples)
+        self._regenerate_vertex_list()
+        self.set_color(self.color)
+        self.actual_sample_index = min(self.actual_sample_index, n_samples-1)
+        
+        self.samples_per_h_division = int(self.n_samples * float(self.grid.h_sep) / float(self.width))
+        self.samples_per_h_division_label.text  = str(float(self.samples_per_h_division*1000/sample_rate))+ "mseg/div"
+
+    def set_amplification(self, amplification):
+        self.amplification = amplification
+        self._regenerate_vertex_list()
+        self.values_per_v_division = int(self.grid.v_sep / float(self.amplification))
+        self.values_per_v_division_label.text = str(self.values_per_v_division)+"/div"
+
+    def set_color(self, color):
+        colors = tuple(flatten([color for x in range(self.n_samples)])) 
+        self._vertex_list.colors = colors
+ 
+    def _regenerate_vertex_list(self):
+        "Regenerates the internal vertex list from self.samples data"
+        self._vertex_list.vertices = self._vertex_list_from_samples(self.samples)
+
+    def _vertex_list_from_samples(self, samples):
+        vertex_list = []
+        for index, sample in enumerate(self.samples):
+            vertex_list.extend(self._vertex_from_sample(sample, index))
+        return vertex_list
+
+    def _vertex_from_sample(self, sample, index):
+        x = self.position[0] + index * self.width / float(self.n_samples)
+        y = self.position[1] + self.heigth/2 + self.samples[index] * self.amplification
+        return  x, y
+
+class StreamWidget(object):
+    def __init__(self, n_samples, size, position):
+        self.graph = StreamGraph(n_samples, size, position, (255,0,90))
+        self.size = size
+
+    def draw(self, samples):
+       self.graph.draw(samples)
+
+    def redraw(self):
+       self.graph.redraw()
+    
+    def set_n_samples(self, n_samples):
+        "Resize samples per widget"
+        self.graph.set_n_samples(n_samples)
